b6ec68b3a9ea65baf9823e82f034a9c7c62ebf5c
typhenebenfriha <typh94@live.fr>
2025-10-27 16:05:55 +0100

    fix mail 2

--- lib/utils/save_file_io.dart
+++ lib/utils/save_file_io.dart
@@ -3,7 +3,8 @@ import 'package:path_provider/path_provider.dart';
 import 'package:versant_event/io_stubs.dart' if (dart.library.io) 'dart:io';
 
 // Saves bytes to the app documents directory and returns the absolute file path.
-Future<String> saveBytesAsFile(Uint8List bytes, {required String filename}) async {
+// Accepts List<int> to be compatible with APIs that return List<int> instead of Uint8List.
+Future<String> saveBytesAsFile(List<int> bytes, {required String filename}) async {
   final dir = await getApplicationDocumentsDirectory();
   final file = File('${dir.path}/$filename');
   await file.writeAsBytes(bytes, flush: true);
--- lib/utils/save_file_web.dart
+++ lib/utils/save_file_web.dart
@@ -2,8 +2,10 @@ import 'dart:typed_data';
 import 'dart:html' as html;
 
 // Triggers a browser download for the given bytes. Returns a pseudo-path token.
-Future<String> saveBytesAsFile(Uint8List bytes, {required String filename}) async {
-  final blob = html.Blob([bytes]);
+// Accepts List<int> and converts to Uint8List for Blob.
+Future<String> saveBytesAsFile(List<int> bytes, {required String filename}) async {
+  final u8 = bytes is Uint8List ? bytes : Uint8List.fromList(bytes);
+  final blob = html.Blob([u8]);
   final url = html.Url.createObjectUrlFromBlob(blob);
   final anchor = html.AnchorElement(href: url)
     ..download = filename
